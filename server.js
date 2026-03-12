a56551a7599978014604137cad817192f72d7772
Sumitkmina <sumitkmina@Sumitkminas-MacBook-Air.local>
2026-03-12 19:26:14 +0530

    fix(release): ship latest flutter apk and disable download caching

--- server.js
+++ server.js
@@ -187,6 +187,12 @@ app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(sanitizeRequestPayload);
 applySecurityHardening(app);
+app.use('/downloads', (req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 function readLeads() {
